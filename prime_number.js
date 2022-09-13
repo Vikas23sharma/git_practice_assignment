a6dcacc69ad8aa6cf867e53c9e4f737404f7fff3
Abhishek Baksi <abhisek.baksi@icloud.com>
2022-09-13 22:07:18 +0530

    This Program Can Check weather a Number Is Prime or Not ?

--- prime_number.js
+++ prime_number.js
@@ -1,7 +1,14 @@
-let num=19;
-let count=0;
-for(i=0;i<=num;i++)
-{if(num%i==0){
-	count++;
-}
-console.log(count);
+   let num=19;
+   let count = 0;
+    
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+            
+        } 
+    }if(count==2){
+            console.log("Yes");
+            
+        }else {
+            console.log("No");
+        }
